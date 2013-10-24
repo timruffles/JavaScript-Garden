667af40a8da24e44f6473e96a2358c0147c76ba5
Tim Ruffles <timruffles@googlemail.com>
2013-10-24 10:56:49 +0100

    updated - removed special cases

--- build.js
+++ build.js
@@ -1,196 +1,96 @@
-var fs = require('fs'),
-    path = require('path'),
-    jade = require('jade'),
-    md = require('marked'),
-    Class = require('neko').Class;
+var fs = require("fs");
+var _ = require("lodash");
+var jade = require('jade');
+var md = require("marked");
 
-var format = new require('fomatto').Formatter();
+function main(opts) {
+  var languages = loadLanguages(opts.languagesDir).sort(function(a,b) {
+      return a.id > b.id ? 1 : -1
+  })
+  languages
+  .forEach(function(lang) {
+    var langBase = lang.id == opts.baseLanguage ? "/" : "/" + lang.id;
+    var out = opts.outDir + langBase + "/index.html";
+    var perLangOpts = _.extend({out: out,languages: languages},opts);
+    outputLanguage(perLangOpts,lang);
+  });
+}
 
+function loadLanguages(path) {
+  var index = readJson(path + "/language.json");
+  var availableListedLanguages = _.intersection(index.listed,fs.readdirSync(path));
+  return availableListedLanguages.reduce(function(all,lang) {
+    var langPath = path + "/" + lang;
+    if(!fs.statSync(langPath).isDirectory()) return all;
+    var data = _.extend(loadLanguage(langPath),{id: lang});
+    return all.concat(data);
+  },[]);
+}
 
-// Garden Generator -------------------------------------------------------------
-// ------------------------------------------------------------------------------
-var Garden = Class(function(options) {
+function loadLanguage(path) {
+  var index = readJson(path + '/index.json');
+  var language = _.extend(index,{
+    sections: index.sections.map(function(section) {
+      return _.extend(section,{
+        link: section.dir,
+        isIntro: section.dir == "intro",
+        articles: section.articles.map(function(article) {
+          return _.extend({
+            link: section.dir + "." + article
+          },loadArticle(path + "/" + section.dir + "/" + article + ".md"));
+        })
+      })
+    })
+  });
+  language.navigation = language.sections;
+  return language;
+}
 
-    this.languages = {};
-    this.options = options;
-    this.options.language = this.json([this.options.dir, 'language.json'].join('/'));
+function loadArticle(path) {
+  var text = fs.readFileSync(path,"utf8");
+  var title = text.substring(0, text.indexOf('\n'));
+  text = text.substring(title.length);
+  title = md(title.replace(/\#/g, '').trim());
+  var titleText = title.substring(3, title.length - 5);
+  text = processArticleBody(text);
 
-    var languages = fs.readdirSync(options.dir);
-    languages.forEach(function(lang) {
-        if (!fs.statSync(this.options.dir + '/' + lang).isDirectory()) return;
-        this.log('Parsing language "{}"...', lang);
-        var langData = {
-            id: lang,
-            navigation: [],
-            index: []
-        };
+  var parts = text.split('<h3>');
+  var subs = parts.map(function(sub,i) {
+    return (i > 0 ? '<h3>' : '') + sub;
+  });
 
-        if (this.loadIndex(langData)) {
-            this.languages[lang] = langData;
-            this.log('    Done.');
+  return {
+    title: titleText,
+    text: text,
+    subs: subs
+  };
+}
 
-        } else {
-            this.log('    Error: Could not find "index.json"!');
-        }
-    },this);
+function processArticleBody(text) {
+    text = md(text).replace(/'/g,'&#39;');
+    text = text.replace(/<blockquote>/g, '<aside>').
+                replace(/<\/blockquote>/g, '</aside>');
 
-    this.log('');
-    this.generateAll();
+    return text.replace(/<aside>\s+<p><strong>ES5/g,
+                        '<aside class="es5"><p><strong>ES5');
+}
 
-}, {
-    log: function() {
-        console.log(format.apply(null, arguments));
-    },
+function readJson(path) {
+  return JSON.parse(fs.readFileSync(path,"utf-8"));
+}
 
-    loadIndex: function(lang) {
-        lang.index = this.json([this.options.dir,
-                                     lang.id, 'index.json'].join('/'));
+function outputLanguage(options,language) {
+  var templateData = _.extend({language: language.id},language,options);
 
-        if (lang.index === null) {
-            return false;
-        }
+  var jadefile = fs.readFileSync(options.template);
+  var jadetemplate = jade.compile(jadefile,{compileDebug: false,debug: false});
+  var html = jadetemplate(templateData);
+  fs.writeFileSync(options.out, html);
+}
 
-        this.lang.title = this.lang.index.langTitle;
-        lang.navigation = [];
-        lang.index.sections.forEach(function(section, i) {
-            this.loadSection(section,lang);
-            this.lang.navigation.push({
-                title: section.title,
-                link: section.dir,
-                articles: section.articles,
-                parsed: section.parsed
-            });
-        },this);
-        return true;
-    },
-
-    loadSection: function(section,lang) {
-        var files = fs.readdirSync(this.folder(section.dir,lang));
-        section.parsed = {};
-        section.link = section.dir;
-
-        section.articles = section.articles || [];
-        section.articles.concat('index').forEach(function(article, e) {
-            if (files.indexOf(article + '.md') !== -1) {
-                var parsed = this.parseArticle(this.md(section.dir, article));
-                section.parsed[article] = parsed;
-                if (section.articles.indexOf(article) !== -1) {
-                    section.articles[e] = {
-                        id: article,
-                        link: section.link + '.' + article,
-                        title: parsed.title,
-                        parsed: parsed
-                    };
-                }
-            }
-        });
-    },
-
-    parseArticle: function(text) {
-        var title = text.substring(0, text.indexOf('\n'));
-        text = text.substring(title.length);
-        title = md(title.replace(/\#/g, '').trim());
-        text = this.toMarkdown(text);
-
-        var parts = text.split('<h3>');
-        var subs = parts.map(function(sub,i) {
-          return i > 0 ? '<h3>' : '') + sub;
-        });
-
-        return {
-            title: title.substring(3, title.length - 4),
-            text: text,
-            subs: subs
-        };
-    },
-
-    toMarkdown: function(text) {
-        text = md(text).replace(/'/g,'&#39;');
-        text = text.replace(/<blockquote>/g, '<aside>').
-                    replace(/<\/blockquote>/g, '</aside>');
-
-        return text.replace(/<aside>\s+<p><strong>ES5/g,
-                            '<aside class="es5"><p><strong>ES5');
-    },
-
-    json: function(file) {
-        try {
-            return JSON.parse(fs.readFileSync(file).toString());
-
-        } catch (err) {
-            return null;
-        }
-    },
-
-    md: function(section, article) {
-        var file = [this.folder(section), article].join('/') + '.md';
-        return fs.readFileSync(file).toString();
-    },
-
-    folder: function(section,lang) {
-        return [this.options.dir, lang.id, section].join('/');
-    },
-
-    render: function(language, template, out) {
-        var lang = this.languages[language];
-        if(!lang) return;
-        this.log('Rendering "{}" to "{}"...', language, out);
-
-        var languages = [];
-        for(var i in this.languages) {
-            if (this.languages.hasOwnProperty(i)) {
-                if (this.options.language.listed.indexOf(i) !== -1) {
-                    languages.push(this.languages[i]);
-                }
-            }
-        }
-
-        var options = {
-            pathPrefix: this.options.pathPrefix,
-            baseLanguage: this.options.language.default,
-            language: language,
-            languages: languages,
-            title: lang.index.title,
-            description: lang.index.description,
-            navigation: lang.navigation,
-            sections: lang.index.sections,
-            top: lang.navigation[0]
-        };
-
-        var jadefile = fs.readFileSync(template);
-        var jadetemplate = jade.compile (jadefile);
-        var html = jadetemplate(options);
-        fs.writeFileSync(out, html);
-        this.log('    Done.');
-    },
-
-    generateAll: function() {
-        for(var i in this.languages) {
-            if (this.languages.hasOwnProperty(i)) {
-                this.generate(i);
-            }
-        }
-    },
-
-    generate: function(lang) {
-        var dir = [this.options.out];
-        if (lang !== this.options.language.default) {
-            dir.push(lang);
-        }
-        dir = dir.join('/');
-
-        path.exists(dir, function(exists) {
-            if (!exists) {
-                fs.mkdirSync(dir, '777');
-            }
-            this.render(lang, this.options.template, dir + '/index.html');
-        }.bind(this));
-    }
-});
-
-exports.build = function (options) {
-    options = options || {dir: 'doc', pathPrefix: 'JavaScript-Garden/', template: 'garden.jade', out: 'site'};
-    new Garden(options);
+exports.build = function (opts) {
+  opts = _.defaults(opts || {},{ languagesDir: "doc", baseLanguage: "en", template: 'garden.jade', pathPrefix: 'JavaScript-Garden/', outDir: "site" });
+  return main(opts);
 }
 
 exports.build();
